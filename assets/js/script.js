a27190e777b5ac3c78a86e31e1753189288c5bcb
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-01-18 12:28:41 +1030

    can fetch city longitude and latitude from city search now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-var APIKey = "68f6f76ac8c1ec3fb198c68558825996"
+/* 
 
-/* When user opens webpage they will see:
+When user opens webpage they will see:
 Text input box that takes city entered and a submit button to search for city weather data
 
 to search for city use openweather api, to get the one call data need longitude and latitude
@@ -12,6 +12,8 @@ which will then be used in fetch from one call on openweather to get todays date
 
 save geocoding data to json and then use variables from this in search fecth for one call api
 
+example geocoding - http://api.openweathermap.org/geo/1.0/direct?q=London,GB&limit=5&appid={API key}
+
 underneath this is an empty area that will be filled by buttons containing past cities searched for, if they are clicked they
 will then search for that city again 
 
@@ -22,6 +24,34 @@ wind speed and uv index. UV index is color coded based on severity.
 Underneath this is next 5 day forecast. 5 cards will be created dynamically showing the following data:
 date, icon of weather, temperature, wind speed and humidity
 
-Cities will be saved to local storage when searched for and this will be what is used to create the button in search list to search again
+Cities will be saved to local storage when searched for and this will be what is used to create the button in search list 
+to search again.
 
 */
+
+var APIKey = "68f6f76ac8c1ec3fb198c68558825996"
+var submitForm = document.querySelector("#search-form")
+
+function getLongLat(event) {
+    event.preventDefault();
+
+    var citySearch = document.querySelector("#city-search").value
+    var queryLongLatURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + citySearch + '&appid=' + APIKey
+
+    console.log(queryLongLatURL)
+
+    var requestUrl = queryLongLatURL;
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        //Using console.log to examine the data
+        console.log(data)
+        console.log(data[0].lat)
+        console.log(data[0].lon);
+        });
+}
+
+submitForm.addEventListener("submit", getLongLat)
